5d2c8d020765c1f61684288f88f9c639671721da
BIYYALA HARI KRISHNA <harikrishnabiyyala@gmail.com>
2023-07-03 11:03:59 +0530

    initial header component done

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBRxxPl4GzfUwH3kHblmTHN6Vf30jQDiBo",
+    authDomain: "clone-efd24.firebaseapp.com",
+    projectId: "clone-efd24",
+    storageBucket: "clone-efd24.appspot.com",
+    messagingSenderId: "376592469049",
+    appId: "1:376592469049:web:fa652457a33594866cd1e8",
+    measurementId: "G-VBPSTTSG3C"
+  };
